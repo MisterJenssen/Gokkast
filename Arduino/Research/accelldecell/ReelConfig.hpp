a5d671657b4dec089d5ad93ae80b4074c903f331
Richard <richardjanssen8@hotmail.com>
2020-07-09 22:37:32 +0200

    Added 3rd wheel to acel/decel testcode

--- Arduino/Research/accelldecell/ReelConfig.hpp
+++ Arduino/Research/accelldecell/ReelConfig.hpp
@@ -11,26 +11,38 @@
 #define reel_2_wire_3_pin 6
 #define reel_2_wire_4_pin 7
 
+#define reel_3_wire_1_pin 0
+#define reel_3_wire_2_pin 1
+#define reel_3_wire_3_pin 12
+#define reel_3_wire_4_pin 13
+
 #define reel_1_light_sensor_pin  2
 #define reel_2_light_sensor_pin  3
+#define reel_3_light_sensor_pin  3
 
 #define reel_1_min_speed 30    //steps/s
 #define reel_2_min_speed 30    //steps/s
+#define reel_3_min_speed 30    //steps/s
 
 #define reel_1_max_speed 300   //steps/s
 #define reel_2_max_speed 300   //steps/s
+#define reel_3_max_speed 300   //steps/s
 
 #define reel_1_min_acceleration 10    //steps/s^2
 #define reel_2_min_acceleration 10    //steps/s^2
+#define reel_3_min_acceleration 10    //steps/s^2
 
 #define reel_1_max_acceleration 100   //steps/s^2
 #define reel_2_max_acceleration 100   //steps/s^2
+#define reel_3_max_acceleration 100   //steps/s^2
 
 #define reel_1_number_of_signs   16
 #define reel_2_number_of_signs   16
+#define reel_3_number_of_signs   16
 
 #define reel_1_steps_per_rev     96
 #define reel_2_steps_per_rev     96
+#define reel_3_steps_per_rev     96
 
 
 #endif /* REELCONFIG_H */
--- Arduino/Research/accelldecell/accelldecell.ino
+++ Arduino/Research/accelldecell/accelldecell.ino
@@ -6,30 +6,38 @@
 
 Reel *reel1;
 Reel *reel2;
+Reel *reel3;
 
-int end_pos1 = -1000;
-int end_pos2 = 1000;
+int end_pos1 = 500;
+int end_pos2 = 750;
+int end_pos3 = 1000;
 
 //void lightGateInterrupt();
 
 void setup()
 {  
-  Serial.begin(baudrate);
+  //Serial.begin(baudrate);  
 
   reel1 = new Reel(reel_1_wire_1_pin, reel_1_wire_2_pin, reel_1_wire_3_pin, reel_1_wire_4_pin, reel_1_light_sensor_pin, reel_1_min_speed, reel_1_max_speed, reel_1_min_acceleration, reel_1_max_acceleration);
   reel2 = new Reel(reel_2_wire_1_pin, reel_2_wire_2_pin, reel_2_wire_3_pin, reel_2_wire_4_pin, reel_2_light_sensor_pin, reel_2_min_speed, reel_2_max_speed, reel_2_min_acceleration, reel_2_max_acceleration);
+  reel3 = new Reel(reel_3_wire_1_pin, reel_3_wire_2_pin, reel_3_wire_3_pin, reel_3_wire_4_pin, reel_3_light_sensor_pin, reel_3_min_speed, reel_3_max_speed, reel_3_min_acceleration, reel_3_max_acceleration);
+
 
   reel1->MoveTo(end_pos1);
   reel2->MoveTo(end_pos2);
+  reel3->MoveTo(end_pos3);
 
   reel1->EnableReel();
   reel2->EnableReel();
+  reel3->EnableReel();
 }
 
 void loop()
 {
    bool reel1_running = reel1->Run();
    bool reel2_running = reel2->Run();
+   bool reel3_running = reel3->Run();
+
    if(reel1->StepsToGo() == 0)
    {
       end_pos1 =  -end_pos1;
@@ -40,7 +48,13 @@ void loop()
       end_pos2 =  -end_pos2;
       reel2->MoveTo(end_pos2);   
    }
+   if(reel3->StepsToGo() == 0)
+   {
+      end_pos3 =  -end_pos3;
+      reel3->MoveTo(end_pos3);   
+   }
 
+   /*
     if (Serial.available()) 
     {
       char inChar = (char)Serial.read();
@@ -48,11 +62,12 @@ void loop()
       {
         reel1->DisableReel();
         reel2->DisableReel();
+        reel3->DisableReel();
 
         Serial.println("Reels stopped");
         while(true);
       }
-    }   
+    } */   
 }
 
 /*
